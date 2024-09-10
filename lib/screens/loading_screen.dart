b635c1ea40da5fe5ae26512fbbdb704b3942579d
xwaynebryan <xwaynelil@gmail.com>
2024-09-10 17:19:45 +0300

    Added the functionality to get the current position of the device using the geolocator package

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:clima/services/location.dart';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({super.key});
@@ -8,11 +9,26 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  // Get the location of the device as soon as the screen loads
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+
+  void getLocation() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+
+    print('Latitude: ${location.latitude}');
+    print('Longitude: ${location.longitude}');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: ElevatedButton(
+        child: TextButton(
           onPressed: () {
             //Get the current location
           },
