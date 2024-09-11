b64ea1dabcc541064504ec6279e49c65378c1684
xwaynebryan <xwaynelil@gmail.com>
2024-09-11 17:50:39 +0300

    Added functionality to fetch weather data from an API

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({super.key});
@@ -24,8 +26,31 @@ class _LoadingScreenState extends State<LoadingScreen> {
     print('Longitude: ${location.longitude}');
   }
 
+  void getWeatherData() async {
+    final uri = Uri.parse(
+        "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid={YOUR API KEY}");
+    http.Response response = await http.get(uri);
+    if (response.statusCode == 200) {
+      String weatherData = response.body;
+      var decodedWeatherData = jsonDecode(
+          weatherData); // Store the decoded data in one variable to avoid repetition
+
+      var theTemperature = decodedWeatherData['main']['temp'];
+      var theConditionNumber = decodedWeatherData['weather'][0]['id'];
+      String cityName = decodedWeatherData['name'];
+
+      // Print each of the pulled data to the screen
+      print(theTemperature);
+      print(theConditionNumber);
+      print(cityName);
+    } else {
+      print(response.statusCode);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    getWeatherData();
     return Scaffold(
       body: Center(
         child: TextButton(
